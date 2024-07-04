75edac5f5546b9366e958d48a661f58cd7b6d58f
highflowey <highflowey@gmail.com>
2024-07-04 15:31:57 +0330

    fixed giving error when file exists

--- src/ctx.rs
+++ src/ctx.rs
@@ -16,25 +16,25 @@ impl<'ctx> Context<'ctx> {
 
     pub fn luau_dir(mut self, dir: Dir<'ctx>) -> Result<Self, LuneWebError> {
         if dir.contains("init.luau") {
+            self.luau = Some(dir);
+            Ok(self)
+        } else {
             Err("The provided Luau directory does not contain 'init.luau'"
                 .to_string()
                 .into())
-        } else {
-            self.luau = Some(dir);
-            Ok(self)
         }
     }
 
     pub fn javascript_dir(mut self, dir: Dir<'ctx>) -> Result<Self, LuneWebError> {
         if dir.contains("index.js") {
+            self.javascript = Some(dir);
+            Ok(self)
+        } else {
             Err(
                 "The provided Javascript directory does not contain 'index.js'"
                     .to_string()
                     .into(),
             )
-        } else {
-            self.javascript = Some(dir);
-            Ok(self)
         }
     }
 }
