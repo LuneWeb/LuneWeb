d0b411d55c098e9a1dcfbc073b9748d45a0df8aa
highflowey <highflowey@gmail.com>
2024-07-04 15:39:51 +0330

    build webview

--- src/app.rs
+++ src/app.rs
@@ -4,6 +4,7 @@ use tao::{
     event_loop::{ControlFlow, EventLoop},
     window::WindowBuilder,
 };
+use wry::WebViewBuilder;
 
 pub struct App<'a> {
     ctx: Context<'a>,
@@ -17,6 +18,20 @@ impl<'app> App<'app> {
     pub fn run(&mut self) -> Result<(), LuneWebError> {
         let event_loop = EventLoop::new();
         let window = WindowBuilder::new().build(&event_loop)?;
+
+        if let Some(javascript_dir) = &self.ctx.javascript {
+            let index = javascript_dir
+                .get_file("index.js")
+                .expect("Failed to find index.js");
+
+            let src = index
+                .contents_utf8()
+                .expect("Failed to interpret file's content as a string");
+
+            WebViewBuilder::new(&window)
+                .with_initialization_script(src)
+                .build()?;
+        }
 
         event_loop.run(move |event, _target, control_flow| {
             if let Event::WindowEvent {
