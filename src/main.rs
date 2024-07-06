08fc6491fbaa038273965af8d7fd6c18160dc30b
highflowey <highflowey@gmail.com>
2024-07-06 14:42:54 +0330

    cli

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use app::App;
+use clap::Parser;
 use ctx::{Context, ContextBuilder};
 use util::{lune_ctx, Error as LuneWebError};
 
@@ -6,13 +7,34 @@ mod app;
 mod ctx;
 mod util;
 
+#[derive(clap::Parser)]
+struct Cli {
+    #[clap(subcommand)]
+    command: SubCommand,
+}
+
+#[derive(clap::Subcommand)]
+enum SubCommand {
+    Run,
+    Build,
+}
+
 #[tokio::main]
 async fn main() {
-    App::from(
-        ContextBuilder::new()
-            .with_lune_ctx(lune_ctx().expect("Failed to create GlobalsContextBuilder")),
-    )
-    .run()
-    .await
-    .expect("Failed to run application");
+    let cli = Cli::parse();
+
+    match cli.command {
+        SubCommand::Run => {
+            App::from(
+                ContextBuilder::new()
+                    .with_lune_ctx(lune_ctx().expect("Failed to create GlobalsContextBuilder")),
+            )
+            .run()
+            .await
+            .expect("Failed to run application");
+        }
+        SubCommand::Build => {
+            unimplemented!()
+        }
+    }
 }
