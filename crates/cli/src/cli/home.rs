06c76527b292ea320d6de9182ebb0e6e977fc1fa
highflowey <highflowey@gmail.com>
2024-07-10 16:08:34 +0330

    update dependencies

--- crates/cli/src/cli/home.rs
+++ crates/cli/src/cli/home.rs
@@ -1,22 +1,19 @@
 use std::{fs, path::PathBuf};
 
+use directories::BaseDirs;
+use once_cell::sync::Lazy;
+
 use crate::{LUAU_TYPES, VERSION};
 
-pub fn luneweb_home() -> Result<PathBuf, String> {
-    let home = match homedir::my_home() {
-        Ok(dir) => dir,
-        Err(err) => return Err(err.to_string()),
-    };
-
-    match home {
-        Some(dir) => Ok(dir),
-        None => Err("Failed to find home directory".to_string()),
-    }
-}
+static HOME_DIR: Lazy<PathBuf> = Lazy::new(|| {
+    BaseDirs::new()
+        .expect("Failed to get base directories")
+        .home_dir()
+        .to_path_buf()
+});
 
 pub fn install_types() -> Result<(), String> {
-    let home = luneweb_home().unwrap();
-    let directory = home.join(".luneweb");
+    let directory = HOME_DIR.join(".luneweb");
     let file_directory = directory.join(format!(".type_defs-{VERSION}.d.luau"));
 
     if !directory.is_dir() {
