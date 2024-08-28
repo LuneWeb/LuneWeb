2a5787bbc40e41abf761c0534fcc8641d7aa9160
highflowey <highflowey@gmail.com>
2024-08-28 20:15:26 +0330

    WindowBuilder accepts table instead of multi args

--- crates/std-window/src/lib.rs
+++ crates/std-window/src/lib.rs
@@ -40,6 +40,25 @@ macro_rules! inner_window {
     };
 }
 
+pub struct LuaWindowConfig {
+    pub title: String,
+    pub url: String,
+    pub dev: bool,
+}
+
+impl<'lua> mlua::FromLua<'lua> for LuaWindowConfig {
+    fn from_lua(value: mlua::Value<'lua>, _: &'lua mlua::Lua) -> mlua::Result<Self> {
+        let t = value
+            .as_table()
+            .ok_or(mlua::Error::runtime("WindowConfig should be a table"))?;
+        let title = t.get("title").unwrap_or("LuneWeb".to_string());
+        let url = t.get("url").unwrap_or("about:blank".to_string());
+        let dev = t.get("dev").unwrap_or(false);
+
+        Ok(Self { title, url, dev })
+    }
+}
+
 pub struct LuaWindow {
     pub(crate) id: WindowId,
     pub message: Rc<LuaMessage>,
@@ -48,14 +67,11 @@ pub struct LuaWindow {
 
 impl LuaWindow {
     #[allow(clippy::new_ret_no_self)]
-    pub fn new(
-        lua: &mlua::Lua,
-        (title, url, dev): (String, String, Option<bool>),
-    ) -> mlua::Result<mlua::Value> {
+    pub fn new(lua: &mlua::Lua, config: LuaWindowConfig) -> mlua::Result<mlua::Value> {
         let window = Window::new(lua)
             .into_lua_err()?
-            .with_title(&title)
-            .with_webview(move |x| x.with_url(&url)?.with_dev(dev.unwrap_or(false)))
+            .with_title(&config.title)
+            .with_webview(move |x| x.with_url(&config.url)?.with_dev(config.dev))
             .into_lua_err()?;
         let id = window.inner.id();
 
