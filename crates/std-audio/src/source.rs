258fe2d89666aabaca1f841f6ba6af4e541637f9
highflowey <highflowey@gmail.com>
2024-09-23 23:14:32 +0330

    get audio duration

--- crates/std-audio/src/source.rs
+++ crates/std-audio/src/source.rs
@@ -1,7 +1,7 @@
 use crate::device::LuaAudioDevice;
 use bstr::BString;
 use mlua::prelude::*;
-use rodio::{Decoder, Sink};
+use rodio::{Decoder, Sink, Source};
 use std::{
     io::{BufReader, Cursor},
     time::Duration,
@@ -9,22 +9,31 @@ use std::{
 
 pub struct LuaAudioSource {
     pub sink: Sink,
+    pub duration: f64,
 }
 
 impl LuaAudioSource {
     pub fn new(lua: &Lua, buffer: BString) -> LuaResult<Self> {
         let reader = BufReader::new(Cursor::new(buffer));
         let source = Decoder::new(reader).into_lua_err()?;
-        let sink = LuaAudioDevice::get(lua)?.sink()?;
+        let duration = source
+            .total_duration()
+            .ok_or(LuaError::runtime(
+                "Audio length is either unknown or infinite",
+            ))?
+            .as_secs_f64();
 
+        let sink = LuaAudioDevice::get(lua)?.sink()?;
         sink.append(source);
 
-        Ok(Self { sink })
+        Ok(Self { sink, duration })
     }
 }
 
 impl LuaUserData for LuaAudioSource {
     fn add_fields<'lua, F: LuaUserDataFields<'lua, Self>>(fields: &mut F) {
+        fields.add_field_method_get("duration", |_, this| Ok(this.duration));
+
         fields.add_field_method_get("position", |_, this| Ok(this.sink.get_pos().as_secs_f64()));
         fields.add_field_method_set("position", |_, this, pos: f64| {
             this.sink
