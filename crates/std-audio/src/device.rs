4ff4697ea15e830513b37d193bf4bcab06d46143
highflowey <highflowey@gmail.com>
2024-09-23 23:27:08 +0330

    give warning instead of error

--- crates/std-audio/src/device.rs
+++ crates/std-audio/src/device.rs
@@ -7,13 +7,24 @@ pub struct LuaAudioDevice {
 
 impl LuaAudioDevice {
     pub fn init(lua: &Lua) -> LuaResult<()> {
-        lua.set_app_data(Self::new(lua, ())?);
+        match Self::new(lua, ()) {
+            Ok(device) => {
+                lua.set_app_data(device);
+            }
+
+            // give a warning instead of an error so the app can work
+            // on machines that dont have audio devices (e.g. when using WSL)
+            Err(_) => println!("[Warn] failed to get an audio device, errors should be expected when using the audio library"),
+        }
 
         Ok(())
     }
 
     pub fn get(lua: &Lua) -> LuaResult<AppDataRef<Self>> {
-        lua.app_data_ref::<LuaAudioDevice>().ok_or(LuaError::runtime("Failed to get AudioDevice from app data container, make sure to call LuaAudioDevice::init before using the audio library"))
+        lua.app_data_ref::<LuaAudioDevice>()
+            .ok_or(LuaError::runtime(
+                "Failed to get AudioDevice from app data container",
+            ))
     }
 
     pub fn new(_: &Lua, _: ()) -> LuaResult<Self> {
