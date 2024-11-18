4a91da155b0215fbc54b6f30c2984cde44d799bb
AshleyFlow <highflowey@gmail.com>
2024-11-18 04:17:48 +0330

    creating window from main thread

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-use std::collections::HashMap;
-
 use mlua::ExternalResult;
+use std::{collections::HashMap, sync::Arc};
 
 #[cfg(any(
     target_os = "linux",
@@ -14,9 +13,16 @@ use tao::platform::unix::EventLoopBuilderExtUnix;
 #[cfg(target_os = "windows")]
 use tao::platform::windows::EventLoopBuilderExtWindows;
 
+#[derive(Debug)]
+pub enum AppProxy {
+    CreateWindow {
+        send_window: flume::Sender<Arc<tao::window::Window>>,
+    },
+}
+
 #[derive(Debug, Default)]
 struct App {
-    windows: HashMap<tao::window::WindowId, tao::window::Window>,
+    windows: HashMap<tao::window::WindowId, Arc<tao::window::Window>>,
 }
 
 impl App {
@@ -31,11 +37,29 @@ impl App {
     }
 
     async fn tick(
-        &self,
-        event: tao::event::Event<'_, ()>,
+        &mut self,
+        target_event: tao::event::Event<'_, AppProxy>,
+        target: &tao::event_loop::EventLoopWindowTarget<AppProxy>,
         control_flow: &mut tao::event_loop::ControlFlow,
-    ) {
-        match event {
+    ) -> mlua::Result<()> {
+        match target_event {
+            tao::event::Event::UserEvent(proxy) => match proxy {
+                AppProxy::CreateWindow { send_window } => {
+                    let window = Arc::new(
+                        tao::window::WindowBuilder::new()
+                            .build(&target)
+                            .into_lua_err()?,
+                    );
+
+                    send_window
+                        .send_async(Arc::clone(&window))
+                        .await
+                        .into_lua_err()?;
+
+                    self.windows.insert(window.id(), window);
+                }
+            },
+
             tao::event::Event::RedrawRequested(id) => {
                 if let Some(window) = self.windows.get(&id) {
                     window.request_redraw();
@@ -62,29 +86,64 @@ impl App {
 
             _ => {}
         }
+
+        Ok(())
     }
 
-    async fn run(mut self) -> mlua::Result<()> {
-        let target = tao::event_loop::EventLoopBuilder::new()
-            .with_any_thread(true)
-            .build();
+    async fn run(
+        mut self,
+    ) -> (
+        tao::event_loop::EventLoopProxy<AppProxy>,
+        std::thread::JoinHandle<()>,
+    ) {
+        let (send_proxy, receive_proxy) = flume::unbounded();
+        let join = std::thread::Builder::new()
+            .name("LuauApp".to_string())
+            .spawn(move || {
+                let target: tao::event_loop::EventLoop<AppProxy> =
+                    tao::event_loop::EventLoopBuilder::with_user_event()
+                        .with_any_thread(true)
+                        .build();
 
-        let window = tao::window::WindowBuilder::new()
-            .build(&target)
-            .into_lua_err()?;
+                send_proxy
+                    .send(target.create_proxy())
+                    .expect("Failed to send proxy to main thread");
 
-        self.windows.insert(window.id(), window);
+                target.run(move |event, target, control_flow| {
+                    // runtime logic here
+                    smol::block_on(self.tick(event, target, control_flow))
+                        .expect("Application tick failed");
+                });
+            })
+            .expect("Failed to spawn a thread for application");
 
-        target.run(move |event, _, control_flow| {
-            // runtime logic here
-            smol::block_on(self.tick(event, control_flow));
-        });
+        let proxy = receive_proxy
+            .recv_async()
+            .await
+            .expect("Failed to receive proxy from application thread");
+
+        (proxy, join)
     }
 }
 
 fn main() {
     let app = App::default();
-    let task = smol::spawn(app.run());
+    let (proxy, join) = smol::block_on(app.run());
 
-    smol::block_on(task).expect("Failed to run application");
+    smol::spawn(async move {
+        let (sender, receiver) = flume::unbounded();
+
+        proxy
+            .send_event(AppProxy::CreateWindow {
+                send_window: sender,
+            })
+            .expect("Failed to send event");
+        let window = receiver.recv().unwrap();
+        println!("{window:?}");
+
+        Ok::<_, mlua::Error>(())
+    })
+    .detach();
+
+    join.join().expect("Failed to join");
 }
