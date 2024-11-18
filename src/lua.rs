25e4f7bed1cd8a5d72144990cb2f0c384979ee53
AshleyFlow <highflowey@gmail.com>
2024-11-18 19:58:36 +0330

    base function for creating window in lua

--- src/lua.rs
+++ src/lua.rs
@@ -1,22 +1,33 @@
 use mlua::prelude::*;
-use std::sync::Arc;
 
 pub fn inject_globals(lua: &Lua) -> LuaResult<()> {
     let globals = lua.globals();
     let task = lua.create_table()?;
+    let app = lua.create_table()?;
 
     task.set(
         "spawn",
         lua.create_function(|lua, (f, args): (mlua::Function, mlua::MultiValue)| {
-            let executor = lua.app_data_ref::<Arc<smol::Executor>>().unwrap();
+            let sched = lua.app_data_ref::<crate::scheduler::Scheduler>().unwrap();
 
-            executor.spawn(f.call_async::<()>(args)).detach();
+            sched.executor.spawn(f.call_async::<()>(args)).detach();
+
+            Ok(())
+        })?,
+    )?;
+
+    app.set(
+        "createWindow",
+        lua.create_function(|lua, _: ()| {
+            let proxy = lua.app_data_ref::<crate::app::AppProxy>().unwrap();
+            let window = proxy.create_window();
 
             Ok(())
         })?,
     )?;
 
     globals.set("task", task)?;
+    globals.set("app", app)?;
 
     Ok(())
 }
