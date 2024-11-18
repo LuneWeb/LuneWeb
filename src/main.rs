cf6fa050a81a45f419242d30e71ce8f3e0c87f3d
AshleyFlow <highflowey@gmail.com>
2024-11-18 13:54:46 +0330

    basic lua instance

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
+use std::sync::Arc;
+
 mod app;
 mod scheduler;
 
 fn main() {
     let app = app::App::default();
-    let scheduler = scheduler::Scheduler::default();
-    let closed_broadcast = app.closed.1.clone();
+    let scheduler = Arc::new(scheduler::Scheduler::default());
 
+    let lua = mlua::Lua::new();
+    let chunk = lua.load(std::fs::read_to_string("app.luau").unwrap());
+
+    let closed_broadcast = app.closed.1.clone();
     let (proxy, join) = smol::block_on(app.run());
 
     scheduler
@@ -16,6 +21,8 @@ fn main() {
             Ok::<_, mlua::Error>(())
         })
         .detach();
+
+    scheduler.spawn(chunk.exec_async()).detach();
 
     scheduler.run(closed_broadcast);
 
