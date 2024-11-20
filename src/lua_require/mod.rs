fb1c5939863c3252e89b1cdc4e1308dda14c5ced
AshleyFlow <highflowey@gmail.com>
2024-11-20 23:39:51 +0330

    load_module function

--- src/lua_require/mod.rs
+++ src/lua_require/mod.rs
@@ -5,6 +5,26 @@ use crate::{
 use mlua::prelude::*;
 use std::path::PathBuf;
 
+async fn load_module(lua: mlua::Lua, path: PathBuf) -> LuaResult<LuaMultiValue> {
+    let contents = smol::fs::read_to_string(&path)
+        .await
+        .map_err(|x| match x.kind() {
+            std::io::ErrorKind::NotFound => mlua::Error::runtime(format!(
+                "The system cannot find {} (os error 2)",
+                path.to_string_lossy()
+            )),
+            _ => x.into_lua_err(),
+        })?;
+
+    let chunk = lua.load(contents).set_name(path.to_string_lossy());
+    let thread = lua.create_thread(chunk.into_function()?)?;
+    let proxy = lua.get_app_proxy();
+
+    proxy.spawn_lua_thread(thread, None);
+
+    Ok(Default::default())
+}
+
 pub async fn lua_require(lua: Lua, path: PathBuf) -> LuaResult<LuaMultiValue> {
     let path = append_extension(clean_path(path), "luau");
 
@@ -13,22 +33,6 @@ pub async fn lua_require(lua: Lua, path: PathBuf) -> LuaResult<LuaMultiValue> {
             "Aliases are not supported in requires yet",
         ))
     } else {
-        let contents = smol::fs::read_to_string(&path)
-            .await
-            .map_err(|x| match x.kind() {
-                std::io::ErrorKind::NotFound => mlua::Error::runtime(format!(
-                    "The system cannot find {} (os error 2)",
-                    path.to_string_lossy()
-                )),
-                _ => x.into_lua_err(),
-            })?;
-
-        let chunk = lua.load(contents).set_name(path.to_string_lossy());
-        let thread = lua.create_thread(chunk.into_function()?)?;
-        let proxy = lua.get_app_proxy();
-
-        proxy.spawn_lua_thread(thread, None);
-
-        Ok(Default::default())
+        load_module(lua, path).await
     }
 }
