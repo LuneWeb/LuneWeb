5ed5e8da30c51bb1d4d53666c7dd091d816b81c3
AshleyFlow <highflowey@gmail.com>
2024-11-21 21:31:27 +0330

    cache modules

--- src/lua_require/mod.rs
+++ src/lua_require/mod.rs
@@ -3,9 +3,56 @@ use crate::{
     LuaAppProxyMethods,
 };
 use mlua::prelude::*;
-use std::path::PathBuf;
+use std::{collections::HashMap, path::PathBuf};
+
+#[derive(Debug, Clone)]
+enum ModuleReturn {
+    Running(flume::Receiver<mlua::Result<mlua::MultiValue>>),
+    Finished(mlua::Result<mlua::MultiValue>),
+}
+
+#[derive(Debug, Clone)]
+struct ModuleCache(HashMap<PathBuf, ModuleReturn>);
+
+fn load_cache(lua: &mlua::Lua) -> mlua::AppDataRef<ModuleCache> {
+    if let Some(cache) = lua.app_data_ref::<ModuleCache>() {
+        cache
+    } else {
+        // lazy load module cache
+        let this = ModuleCache(Default::default());
+        lua.set_app_data(this);
+        lua.app_data_ref::<ModuleCache>().unwrap()
+    }
+}
 
 async fn load_module(lua: mlua::Lua, path: PathBuf) -> LuaResult<LuaMultiValue> {
+    let cached_module = {
+        let cache = load_cache(&lua);
+        cache.0.get(&path).map(|x| x.to_owned())
+    };
+
+    if let Some(module) = cached_module {
+        match module {
+            ModuleReturn::Running(channel) => {
+                return channel.recv_async().await.into_lua_err()?;
+            }
+            ModuleReturn::Finished(result) => {
+                return result.to_owned();
+            }
+        }
+    }
+
+    let cache_sender = {
+        let (sender, receiver) = flume::unbounded();
+
+        let mut cache = lua.app_data_mut::<ModuleCache>().unwrap();
+        cache
+            .0
+            .insert(path.clone(), ModuleReturn::Running(receiver));
+
+        sender
+    };
+
     let contents = smol::fs::read_to_string(&path)
         .await
         .map_err(|x| match x.kind() {
@@ -21,7 +68,20 @@ async fn load_module(lua: mlua::Lua, path: PathBuf) -> LuaResult<LuaMultiValue> 
     let proxy = lua.get_app_proxy();
 
     proxy.spawn_lua_thread(thread.clone(), None);
+
     let result = proxy.await_lua_thread(thread).await;
+
+    {
+        cache_sender
+            .send_async(result.clone())
+            .await
+            .into_lua_err()?;
+
+        let mut cache = lua.app_data_mut::<ModuleCache>().unwrap();
+        cache
+            .0
+            .insert(path.clone(), ModuleReturn::Finished(result.clone()))
+    };
 
     result
 }
