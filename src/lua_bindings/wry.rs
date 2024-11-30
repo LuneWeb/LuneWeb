3170ab823f9758904367e139221458a60bae3210
AshleyFlow <highflowey@gmail.com>
2024-11-30 18:47:57 +0330

    url property

--- src/lua_bindings/wry.rs
+++ src/lua_bindings/wry.rs
@@ -7,6 +7,11 @@ unsafe impl Send for LuaWebView {}
 impl mlua::UserData for LuaWebView {
     fn add_fields<F: mlua::UserDataFields<Self>>(fields: &mut F) {
         fields.add_field_method_get("id", |_, this| Ok(this.0.id().to_string()));
+
+        fields.add_field_method_get("url", |_, this| this.0.url().into_lua_err());
+        fields.add_field_method_set("url", |_, this, url: String| {
+            this.0.load_url(&url).into_lua_err()
+        });
     }
 
     fn add_methods<M: UserDataMethods<Self>>(methods: &mut M) {
@@ -18,10 +23,6 @@ impl mlua::UserData for LuaWebView {
 
         methods.add_method("loadHtml", |_, this, html: String| {
             this.0.load_html(&html).into_lua_err()
-        });
-
-        methods.add_method("loadUrl", |_, this, url: String| {
-            this.0.load_url(&url).into_lua_err()
         });
     }
 }
