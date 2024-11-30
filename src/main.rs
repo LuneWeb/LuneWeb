294037f8ee3512053ee717bdf2f6795ea48ec9a0
AshleyFlow <highflowey@gmail.com>
2024-11-30 15:07:57 +0330

    spawn thread after lua globals are ready

--- src/main.rs
+++ src/main.rs
@@ -23,9 +23,20 @@ impl LuaAppProxyMethods for mlua::Lua {
     }
 }
 
-fn main() {
+fn main() -> mlua::Result<()> {
     let sched = Scheduler::new();
     let lua = mlua::Lua::new();
+
+    lua.globals()
+        .set("task", lua_std::StandardLibrary::Task.into_lua(&lua)?)?;
+
+    lua.globals()
+        .set("web", lua_std::StandardLibrary::Web.into_lua(&lua)?)?;
+
+    lua.globals().set(
+        "require",
+        lua.create_async_function(lua_require::lua_require)?,
+    )?;
 
     // keep lua alive until the end of the scope
     let _lua = lua.clone();
@@ -33,6 +44,9 @@ fn main() {
     scheduler::thread::initialize_threads(sched.clone(), |proxy| {
         if let Err(err) = smol::block_on::<mlua::Result<()>>(async move {
             let script_path = std::env::args().nth(1).unwrap_or("init.luau".to_string());
+
+            lua.set_app_data(proxy.clone());
+
             sched
                 .executor
                 .spawn(lua_require::utils::load_script(
@@ -41,23 +55,12 @@ fn main() {
                 ))
                 .detach();
 
-            lua.set_app_data(proxy.clone());
-
-            lua.globals()
-                .set("task", lua_std::StandardLibrary::Task.into_lua(&lua)?)?;
-
-            lua.globals()
-                .set("web", lua_std::StandardLibrary::Web.into_lua(&lua)?)?;
-
-            lua.globals().set(
-                "require",
-                lua.create_async_function(lua_require::lua_require)?,
-            )?;
-
             Ok(())
         }) {
             eprintln!("{err}");
             std::process::exit(1);
         };
     });
+
+    Ok(())
 }
