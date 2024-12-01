1506c28516ab4dbde90ae6ef00c9783e1f8ba7c5
AshleyFlow <highflowey@gmail.com>
2024-12-01 04:17:23 +0330

    basic protocols impl

--- src/lua_std/web.rs
+++ src/lua_std/web.rs
@@ -1,16 +1,19 @@
 use crate::{
     lua_bindings::{tao::LuaWindow, wry::LuaWebView},
+    scheduler::thread::LuaThreadMethods,
     utils::table_builder::TableBuilder,
     LuaAppProxyMethods,
 };
 use mlua::{ExternalResult, IntoLua};
-use std::sync::Arc;
+use std::{collections::HashMap, sync::Arc};
 use tao::window::Window;
+use wry::http::Response;
 
 #[derive(Debug, Clone)]
 pub struct WebViewProps {
     pub parent: Arc<Window>,
     pub scripts: Vec<String>,
+    pub servers: HashMap<String, mlua::Function>,
 }
 
 impl mlua::FromLua for WebViewProps {
@@ -29,24 +32,61 @@ impl mlua::FromLua for WebViewProps {
         Ok(Self {
             parent,
             scripts: t.get("scripts").unwrap_or_default(),
+            servers: t.get("servers").unwrap_or_default(),
         })
     }
 }
 
 pub(super) fn create(lua: &mlua::Lua) -> mlua::Result<mlua::Value> {
     TableBuilder::new(lua)?
-        .with_async_function(
-            "createWindow",
-            move |lua, title: Option<String>| async move {
-                let window = lua.get_app_proxy().create_window(title).await;
-                Ok(LuaWindow(window))
-            },
-        )?
-        .with_function("createWebView", move |_, props: WebViewProps| {
+        .with_async_function("createWindow", |lua, title: Option<String>| async move {
+            let window = lua.get_app_proxy().create_window(title).await;
+            Ok(LuaWindow(window))
+        })?
+        .with_async_function("createWebView", |lua, props: WebViewProps| async move {
             let mut builder = wry::WebViewBuilder::new();
 
             for script in props.scripts {
                 builder = builder.with_initialization_script(&script);
+            }
+
+            for server in props.servers {
+                let lua_inner = lua.clone();
+
+                builder =
+                    builder.with_asynchronous_custom_protocol(server.0, move |_, req, res| {
+                        let thread = lua_inner
+                            .create_thread(server.1.clone())
+                            .expect("Failed to turn callback into thread");
+
+                        let proxy = lua_inner.get_app_proxy();
+
+                        let lua = lua_inner.clone();
+                        lua_inner
+                            .spawn(async move {
+                                proxy.spawn_lua_thread(
+                                    thread.clone(),
+                                    Some(mlua::MultiValue::from_vec(vec![String::from_utf8(
+                                        req.into_body(),
+                                    )
+                                    .unwrap()
+                                    .into_lua(&lua)
+                                    .unwrap()])),
+                                );
+                                res.respond::<Vec<u8>>(Response::new(
+                                    proxy
+                                        .await_lua_thread(thread)
+                                        .await
+                                        .expect("Server callback failed to give results")
+                                        .get(0)
+                                        .unwrap()
+                                        .as_string_lossy()
+                                        .unwrap()
+                                        .into(),
+                                ));
+                            })
+                            .detach();
+                    });
             }
 
             let webview = builder.build(&props.parent).into_lua_err()?;
