ebc1e1d67b5dc8ad1b566609c57d6fbfcef30f03
AshleyFlow <highflowey@gmail.com>
2024-12-01 17:18:47 +0330

    LuaWebViewBuilder

--- src/lua_bindings/wry.rs
+++ src/lua_bindings/wry.rs
@@ -1,4 +1,82 @@
-use mlua::{ExternalResult, LuaSerdeExt, UserDataMethods};
+use super::tao::LuaWindow;
+use crate::{scheduler::thread::LuaThreadMethods, LuaAppProxyMethods};
+use mlua::{ExternalResult, IntoLua, LuaSerdeExt, UserDataMethods};
+use std::sync::Arc;
+use tao::window::Window;
+use wry::http::Response;
+
+pub struct LuaWebViewBuilder(pub wry::WebViewBuilder<'static>);
+
+unsafe impl Send for LuaWebViewBuilder {}
+
+impl mlua::UserData for LuaWebViewBuilder {
+    fn add_methods<M: UserDataMethods<Self>>(methods: &mut M) {
+        methods.add_function(
+            "withInitScript",
+            |_, (this_userdata, script): (mlua::AnyUserData, String)| {
+                let mut this = this_userdata.take::<Self>()?;
+
+                this.0 = this.0.with_initialization_script(&script);
+
+                Ok(this)
+            },
+        );
+
+        methods.add_async_function(
+            "withCustomProtocol",
+            |lua, (this_userdata, name, callback): (mlua::AnyUserData, String, mlua::Function)| async move {
+                let mut this = this_userdata.take::<Self>()?;
+
+                this.0 = this
+                    .0
+                    .with_asynchronous_custom_protocol(name, move |_, req, res| {
+                        let lua_inner = lua.clone();
+                        let proxy = lua.get_app_proxy();
+                        let thread = lua
+                            .create_thread(callback.clone())
+                            .expect("Failed to turn callback into thread");
+
+                        lua.spawn(async move {
+                            proxy.spawn_lua_thread(
+                                thread.clone(),
+                                Some(mlua::MultiValue::from_vec(vec![String::from_utf8(
+                                    req.into_body(),
+                                )
+                                .unwrap()
+                                .into_lua(&lua_inner)
+                                .unwrap()])),
+                            );
+                            res.respond::<Vec<u8>>(Response::new(
+                                proxy
+                                    .await_lua_thread(thread)
+                                    .await
+                                    .expect("Server callback failed to give results")
+                                    .get(0)
+                                    .unwrap()
+                                    .as_string_lossy()
+                                    .unwrap()
+                                    .into(),
+                            ));
+                        })
+                        .detach();
+                    });
+
+                Ok(this)
+            },
+        );
+
+        methods.add_function(
+            "build",
+            |_, (this_userdata, parent): (mlua::AnyUserData, mlua::AnyUserData)| {
+                let this = this_userdata.take::<Self>()?;
+                let parent =
+                    parent.borrow_scoped::<LuaWindow, Arc<Window>>(|window| window.0.clone())?;
+
+                Ok(LuaWebView(this.0.build(&parent).into_lua_err()?))
+            },
+        );
+    }
+}
 
 pub struct LuaWebView(pub wry::WebView);
 
--- src/lua_std/web.rs
+++ src/lua_std/web.rs
@@ -1,41 +1,9 @@
 use crate::{
-    lua_bindings::{tao::LuaWindow, wry::LuaWebView},
-    scheduler::thread::LuaThreadMethods,
+    lua_bindings::{tao::LuaWindow, wry::LuaWebViewBuilder},
     utils::table_builder::TableBuilder,
     LuaAppProxyMethods,
 };
-use mlua::{ExternalResult, IntoLua};
-use std::{collections::HashMap, sync::Arc};
-use tao::window::Window;
-use wry::http::Response;
-
-#[derive(Debug, Clone)]
-pub struct WebViewProps {
-    pub parent: Arc<Window>,
-    pub scripts: Vec<String>,
-    pub servers: HashMap<String, mlua::Function>,
-}
-
-impl mlua::FromLua for WebViewProps {
-    fn from_lua(value: mlua::Value, _: &mlua::Lua) -> mlua::Result<Self> {
-        let t = value
-            .as_table()
-            .ok_or_else(|| mlua::Error::FromLuaConversionError {
-                from: value.type_name(),
-                to: "table".to_string(),
-                message: None,
-            })?;
-
-        let parent = t.get::<mlua::AnyUserData>("parent")?;
-        let parent = parent.borrow_scoped::<LuaWindow, Arc<Window>>(|window| window.0.clone())?;
-
-        Ok(Self {
-            parent,
-            scripts: t.get("scripts").unwrap_or_default(),
-            servers: t.get("servers").unwrap_or_default(),
-        })
-    }
-}
+use mlua::IntoLua;
 
 pub(super) fn create(lua: &mlua::Lua) -> mlua::Result<mlua::Value> {
     TableBuilder::new(lua)?
@@ -43,55 +11,8 @@ pub(super) fn create(lua: &mlua::Lua) -> mlua::Result<mlua::Value> {
             let window = lua.get_app_proxy().create_window(title).await;
             Ok(LuaWindow(window))
         })?
-        .with_async_function("createWebView", |lua, props: WebViewProps| async move {
-            let mut builder = wry::WebViewBuilder::new();
-
-            for script in props.scripts {
-                builder = builder.with_initialization_script(&script);
-            }
-
-            for server in props.servers {
-                let lua_inner = lua.clone();
-
-                builder =
-                    builder.with_asynchronous_custom_protocol(server.0, move |_, req, res| {
-                        let thread = lua_inner
-                            .create_thread(server.1.clone())
-                            .expect("Failed to turn callback into thread");
-
-                        let proxy = lua_inner.get_app_proxy();
-
-                        let lua = lua_inner.clone();
-                        lua_inner
-                            .spawn(async move {
-                                proxy.spawn_lua_thread(
-                                    thread.clone(),
-                                    Some(mlua::MultiValue::from_vec(vec![String::from_utf8(
-                                        req.into_body(),
-                                    )
-                                    .unwrap()
-                                    .into_lua(&lua)
-                                    .unwrap()])),
-                                );
-                                res.respond::<Vec<u8>>(Response::new(
-                                    proxy
-                                        .await_lua_thread(thread)
-                                        .await
-                                        .expect("Server callback failed to give results")
-                                        .get(0)
-                                        .unwrap()
-                                        .as_string_lossy()
-                                        .unwrap()
-                                        .into(),
-                                ));
-                            })
-                            .detach();
-                    });
-            }
-
-            let webview = builder.build(&props.parent).into_lua_err()?;
-
-            Ok(LuaWebView(webview))
+        .with_function("createWebView", |_, _: ()| {
+            Ok(LuaWebViewBuilder(wry::WebViewBuilder::new()))
         })?
         .build_readonly()?
         .into_lua(lua)
